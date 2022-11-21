0afa442d64bc445f3732c8b6699ffc82303db86c
dafiulh <dafiulhaq97@gmail.com>
2022-11-21 17:52:27 +0700

    Frontend dev (#1)
    * add leaflet tailwind alpine
    
    * update ui

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: [
+    './index.html'
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        'body': 'Inter, Helvetica, Arial, sans-serif'
+      },
+      colors: {
+        'gmf-blue': '#23315a',
+        'gmf-white': '#f5f5f5',
+        'gmf-yellow': '#fdce11'
+      }
+    },
+  }
+};
