4de9eeb2375a66038efd809a68ba945741ed24b8
Dafiul Haq <dafiulhaq97@gmail.com>
2022-11-24 14:24:47 +0700

    restructure backend folder

--- backend/app.js
+++ backend/app.js
@@ -2,7 +2,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var indexRouter = require('./routes/index');
+var indexRouter = require('./routes');
 
 var app = express();
 
@@ -12,7 +12,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 if (process.argv[2] !== '--dev') {
-  app.use(express.static(path.join(__dirname, 'frontend/dist')));
+  app.use(express.static(path.join(__dirname, '../frontend/dist')));
 }
 
 app.use('/api', indexRouter);
